51264c3fef3704235ba553f06afa10fed529eccc
Yuan <joseftien@gmail.com>
2015-03-15 20:34:40 -0700

    test

--- InterviewProblems/src/array/ArrayTest.java
+++ InterviewProblems/src/array/ArrayTest.java
@@ -4,13 +4,13 @@ import array.NextPermutation.Solution;
 
 public class ArrayTest {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-	Solution haha = new NextPermutation().new Solution();
-	haha.nextPermutation(new int[] { 3, 2, 1 });
-	
-	System.out.println('5' - '0');
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Solution haha = new NextPermutation().new Solution();
+		haha.nextPermutation(new int[] { 3, 2, 1 });
+
+		System.out.println('5' - '0');
+	}
 }
