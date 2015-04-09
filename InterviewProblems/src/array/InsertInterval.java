45072ee9d34bdd13f87f7d80cb924c2ef8ef864a
Yuan <joseftien@gmail.com>
2015-04-09 04:36:48 -0700

    2015-04-09

--- InterviewProblems/src/array/InsertInterval.java
+++ InterviewProblems/src/array/InsertInterval.java
@@ -1,106 +1,135 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Insert Interval
+ * 
+ * Given a set of non-overlapping intervals, insert a new interval into the
+ * intervals (merge if necessary).
+ * 
+ * You may assume that the intervals were initially sorted according to their
+ * start times.
+ * 
+ * Example 1: Given intervals [1,3],[6,9], insert and merge [2,5] in as
+ * [1,5],[6,9].
+ * 
+ * Example 2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in
+ * as [1,2],[3,10],[12,16].
+ * 
+ * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+ */
 
 public class InsertInterval {
-    /**
-     * Insert Interval
-     * 
-     * Given a set of non-overlapping intervals, insert a new interval into the
-     * intervals (merge if necessary).
-     * 
-     * You may assume that the intervals were initially sorted according to
-     * their start times.
-     * 
-     * Example 1: Given intervals [1,3],[6,9], insert and merge [2,5] in as
-     * [1,5],[6,9].
-     * 
-     * Example 2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9]
-     * in as [1,2],[3,10],[12,16].
-     * 
-     * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
-     */
 
     /*
-     * Difficult to write bug free code. Lots of detail.
-     * 
-     * use high hand's solution
+     * Same algorithm as the 2013 solution
      */
+    public class Solution {
+	public List<Interval> insert(List<Interval> intervals,
+		Interval newInterval) {
+	    List<Interval> res = new ArrayList<Interval>();
+	    if (intervals == null || intervals.size() == 0
+		    || newInterval == null) {
+		res.add(newInterval);
+		return res;
+	    }
 
-    public static class Interval {
-	int start;
-	int end;
+	    if (newInterval.end < intervals.get(0).start) {
+		res.add(newInterval);
+		res.addAll(intervals);
+		return res;
+	    } else if (newInterval.start > intervals.get(intervals.size() - 1).end) {
+		res.addAll(intervals);
+		res.add(newInterval);
+		return res;
+	    }
 
-	public Interval() {
-	    start = 0;
-	    end = 0;
-	}
+	    int i = 0;
+	    boolean hasInserted = false;
+	    while (i < intervals.size()) {
+		Interval cur = intervals.get(i);
 
-	public Interval(int s, int e) {
-	    start = s;
-	    end = e;
-	}
+		if (!hasInserted && cur.end >= newInterval.start) {
+		    newInterval.start = Math.min(newInterval.start, cur.start);
 
-	public void print() {
-	    System.out.println(this.start + ", " + this.end);
+		    while (i < intervals.size()
+			    && intervals.get(i).start <= newInterval.end) {
+			newInterval.end = Math.max(newInterval.end,
+				intervals.get(i).end);
+			i++;
+		    }
+
+		    res.add(newInterval);
+		    hasInserted = true;
+		} else {
+		    res.add(cur);
+		    i++;
+		}
+	    }
+
+	    return res;
 	}
     }
 
-    public static ArrayList<Interval> insert(ArrayList<Interval> intervals,
-	    Interval newInterval) {
-	ArrayList<Interval> result = new ArrayList<Interval>();
+    class Solution_2013 {
+	public ArrayList<Interval> insert(ArrayList<Interval> intervals,
+		Interval newInterval) {
+	    ArrayList<Interval> result = new ArrayList<Interval>();
 
-	if (intervals == null || intervals.isEmpty()) {
-	    if (newInterval == null)
-		return result;
-	    result.add(newInterval);
-	    return result;
-	}
-
-	/*
-	 * handle two boundary conditions separately:
-	 * 
-	 * 1. if the new interval is ahead of all intervals in array
-	 * 
-	 * 2. if the new interval is behind all intervals in array
-	 */
-	if (newInterval.end < intervals.get(0).start) {
-	    result.add(newInterval);
-	    result.addAll(intervals);
-	    return result;
-	} else if (newInterval.start > intervals.get(intervals.size() - 1).end) {
-	    result.addAll(intervals);
-	    result.add(newInterval);
-	    return result;
-	}
-
-	boolean insertStart = false;
-	int i = 0;
-
-	/*
-	 * two nested while loops.
-	 * 
-	 * Create a newInterval and keep updating its end
-	 */
-	while (i < intervals.size()) {
-	    Interval curInterval = intervals.get(i);
-	    if (!insertStart && curInterval.end >= newInterval.start) {
-		newInterval.start = Math.min(curInterval.start,
-			newInterval.start);
-		insertStart = true;
-		while (i < intervals.size()
-			&& intervals.get(i).start <= newInterval.end) {
-		    newInterval.end = Math.max(newInterval.end,
-			    intervals.get(i).end);
-		    i++;
-		}
+	    if (intervals == null || intervals.isEmpty()) {
+		if (newInterval == null)
+		    return result;
 		result.add(newInterval);
-		continue;
+		return result;
 	    }
-	    i++;
-	    result.add(curInterval);
-	}
 
-	return result;
+	    /*
+	     * handle two boundary conditions separately:
+	     * 
+	     * 1. if the new interval is ahead of all intervals in array
+	     * 
+	     * 2. if the new interval is behind all intervals in array
+	     */
+	    if (newInterval.end < intervals.get(0).start) {
+		result.add(newInterval);
+		result.addAll(intervals);
+		return result;
+	    } else if (newInterval.start > intervals.get(intervals.size() - 1).end) {
+		result.addAll(intervals);
+		result.add(newInterval);
+		return result;
+	    }
+
+	    boolean insertStart = false;
+	    int i = 0;
+
+	    /*
+	     * two nested while loops.
+	     * 
+	     * Create a newInterval and keep updating its end
+	     */
+	    while (i < intervals.size()) {
+		Interval curInterval = intervals.get(i);
+		if (!insertStart && curInterval.end >= newInterval.start) {
+		    newInterval.start = Math.min(curInterval.start,
+			    newInterval.start);
+		    insertStart = true;
+		    while (i < intervals.size()
+			    && intervals.get(i).start <= newInterval.end) {
+			newInterval.end = Math.max(newInterval.end,
+				intervals.get(i).end);
+			i++;
+		    }
+		    result.add(newInterval);
+		    continue;
+		}
+		i++;
+		result.add(curInterval);
+	    }
+
+	    return result;
+	}
     }
 }
--- InterviewProblems/src/tree/MinimumDepthOfBinaryTree.java
+++ InterviewProblems/src/tree/MinimumDepthOfBinaryTree.java
@@ -1,48 +1,79 @@
 package tree;
 
+/**
+ * Minimum Depth of Binary Tree
+ * 
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ */
+
+/*
+ * Be careful of the definition of "leaf node"
+ * 
+ * Two stoping conditions: while root is null and while root is a leaf
+ */
 public class MinimumDepthOfBinaryTree {
-    /**
-     * Minimum Depth of Binary Tree
-     * 
-     * Given a binary tree, find its minimum depth.
-     * 
-     * The minimum depth is the number of nodes along the shortest path from the
-     * root node down to the nearest leaf node.
-     */
-    /*
-     * Be careful of the definition of "leaf node"
-     * 
-     * Two stoping conditions: while root is null and while root is a leaf
-     */
-    public static int minDepth(TreeNode root) {
-	if (root == null)
-	    return 0;
 
-	if (root.left == null && root.right == null)
-	    return 1;
+    public class Solution {
+	public int minDepth(TreeNode root) {
+	    if (root == null) {
+		return 0;
+	    }
 
-	int left = Integer.MAX_VALUE;
-	int right = Integer.MAX_VALUE;
-
-	if (root.left != null) {
-	    left = minDepth(root.left);
-	}
-	if (root.right != null) {
-	    right = minDepth(root.right);
+	    return dfs(root);
 	}
 
-	return 1 + Math.min(left, right);
+	private int dfs(TreeNode root) {
+	    if (root.left == null && root.right == null) {
+		return 1;
+	    }
+
+	    int left = Integer.MAX_VALUE;
+	    if (root.left != null) {
+		left = dfs(root.left);
+	    }
+	    int right = Integer.MAX_VALUE;
+	    if (root.right != null) {
+		right = dfs(root.right);
+	    }
+
+	    return Math.min(left, right) + 1;
+	}
     }
 
-    /*
-     * For reference: get height (maximum depth) of a binary tree
-     * 
-     * No need to care every leaf
-     */
+    class Solution_2013 {
+	public int minDepth(TreeNode root) {
+	    if (root == null)
+		return 0;
 
-    public int getHeight(TreeNode root) {
-	if (root == null)
-	    return 0;
-	return (Math.max(getHeight(root.left), getHeight(root.right)) + 1);
+	    if (root.left == null && root.right == null)
+		return 1;
+
+	    int left = Integer.MAX_VALUE;
+	    int right = Integer.MAX_VALUE;
+
+	    if (root.left != null) {
+		left = minDepth(root.left);
+	    }
+	    if (root.right != null) {
+		right = minDepth(root.right);
+	    }
+
+	    return 1 + Math.min(left, right);
+	}
+
+	/*
+	 * For reference: get height (maximum depth) of a binary tree
+	 * 
+	 * No need to care every leaf
+	 */
+
+	public int getHeight(TreeNode root) {
+	    if (root == null)
+		return 0;
+	    return (Math.max(getHeight(root.left), getHeight(root.right)) + 1);
+	}
     }
 }
