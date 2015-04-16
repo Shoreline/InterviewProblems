6457a9c7e1657e00546227d497306ac7d7bfe3f7
Yuan <joseftien@gmail.com>
2015-04-16 09:45:37 -0700

    2015-04-16

--- InterviewProblems/src/tree/ConvertSortedArrayToBinarySearchTree.java
+++ InterviewProblems/src/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -1,33 +1,37 @@
 package tree;
 
+/**
+ * Convert Sorted Array to Binary Search Tree
+ * 
+ * Given an array where elements are sorted in ascending order, convert it to a
+ * height balanced BST.
+ * 
+ */
+
+/*
+ * Similar to construct binary tree from xx and xx traversal, but easier. 
+ */
 public class ConvertSortedArrayToBinarySearchTree {
-    /**
-     * Convert Sorted Array to Binary Search Tree
-     * 
-     * Given an array where elements are sorted in ascending order, convert it
-     * to a height balanced BST.
-     */
 
-    public static TreeNode sortedArrayToBST(int[] num) {
-	if (num == null || num.length == 0)
-	    return null;
+    public class Solution {
+	public TreeNode sortedArrayToBST(int[] num) {
+	    if (num == null || num.length == 0) {
+		return null;
+	    }
 
-	TreeNode root = sortedArrayToBST(num, 0, num.length - 1);
+	    return treeBuilder(num, 0, num.length - 1);
+	}
 
-	return root;
-    }
+	private TreeNode treeBuilder(int[] num, int start, int end) {
+	    if (start > end) {
+		return null;
+	    }
+	    int index = (start + end) / 2;
+	    TreeNode root = new TreeNode(num[index]);
+	    root.left = treeBuilder(num, start, index - 1);
+	    root.right = treeBuilder(num, index + 1, end);
 
-    private static TreeNode sortedArrayToBST(int[] num, int start, int end) {
-
-	if (start > end)
-	    return null;
-
-	int mid = (start + end) / 2;
-	TreeNode root = new TreeNode(num[mid]);
-
-	root.left = sortedArrayToBST(num, start, mid - 1);
-	root.right = sortedArrayToBST(num, mid + 1, end);
-
-	return root;
+	    return root;
+	}
     }
 }
--- InterviewProblems/src/tree/ConvertSortedListToBinarySearchTree.java
+++ InterviewProblems/src/tree/ConvertSortedListToBinarySearchTree.java
@@ -1,88 +1,137 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import linkedlist.ListNode;
 
+/**
+ * Convert Sorted List to Binary Search Tree
+ * 
+ * Given a singly linked list where elements are sorted in ascending order,
+ * convert it to a height balanced BST.
+ */
+
 public class ConvertSortedListToBinarySearchTree {
-    /**
-     * Convert Sorted List to Binary Search Tree
+    /*
+     * The in-order traversal for a BST is a sorted sequence
      * 
-     * Given a singly linked list where elements are sorted in ascending order,
-     * convert it to a height balanced BST.
+     * -> similarly, from left to right to traverse a sorted sequence has the
+     * same order as in-order BST traversal.
      */
+    public class Solution {
+	public TreeNode sortedListToBST(ListNode head) {
+	    if (head == null) {
+		return null;
+	    }
+	    // always only 1 element
+	    List<ListNode> headList = new ArrayList<ListNode>();
+	    headList.add(head);
+
+	    int len = 0;
+	    ListNode cur = head;
+	    while (cur != null) {
+		cur = cur.next;
+		len++;
+	    }
+
+	    return treeBuilder(headList, 0, len - 1);
+	}
+
+	private TreeNode treeBuilder(List<ListNode> head, int start, int end) {
+	    if (start > end) {
+		return null;
+	    }
+
+	    // in-order traverse. Go for the left child first.
+	    TreeNode left = treeBuilder(head, start, (start + end) / 2 - 1);
+
+	    ListNode headNode = head.get(0);
+	    TreeNode root = new TreeNode(headNode.val);
+	    root.left = left;
+	    // headNode=headNode.next; // this does not work!
+	    head.set(0, headNode.next);
+
+	    TreeNode right = treeBuilder(head, (start + end) / 2 + 1, end);
+	    root.right = right;
+
+	    return root;
+	}
+    }
 
     /*
      * Just convert the list to be an array or ArrayList -> memory limit
      * exceeded
      */
+    class method {
+	public TreeNode sortedListToBSTB(ListNode head) {
+	    if (head == null)
+		return null;
 
-    public static TreeNode sortedListToBSTB(ListNode head) {
-	if (head == null)
-	    return null;
+	    ArrayList<Integer> list = new ArrayList<Integer>();
+	    ListNode curNode = head;
+	    while (curNode != null) {
+		list.add(curNode.val);
+		curNode = curNode.next;
+	    }
 
-	ArrayList<Integer> list = new ArrayList<Integer>();
-	ListNode curNode = head;
-	while (curNode != null) {
-	    list.add(curNode.val);
-	    curNode = curNode.next;
+	    TreeNode result = sortedListToBSTB(list, 0, list.size() - 1);
+
+	    return result;
 	}
 
-	TreeNode result = sortedListToBSTB(list, 0, list.size() - 1);
+	private TreeNode sortedListToBSTB(ArrayList<Integer> list, int start,
+		int end) {
+	    if (start > end)
+		return null;
 
-	return result;
-    }
+	    int mid = (start + end) / 2;
+	    TreeNode root = new TreeNode(list.get(mid));
+	    root.left = sortedListToBSTB(list, start, mid - 1);
+	    root.right = sortedListToBSTB(list, mid + 1, end);
 
-    private static TreeNode sortedListToBSTB(ArrayList<Integer> list,
-	    int start, int end) {
-	if (start > end)
-	    return null;
-
-	int mid = (start + end) / 2;
-	TreeNode root = new TreeNode(list.get(mid));
-	root.left = sortedListToBSTB(list, start, mid - 1);
-	root.right = sortedListToBSTB(list, mid + 1, end);
-
-	return root;
+	    return root;
+	}
     }
 
     /*
      * 
      * So , have to use an alternative way
      */
+    class method_2 {
+	public TreeNode sortedListToBST(ListNode head) {
+	    if (head == null)
+		return null;
 
-    public TreeNode sortedListToBST(ListNode head) {
-	if (head == null)
-	    return null;
+	    ListNode curNode = head;
+	    int length = 0; // start from 0
+	    while (curNode != null) {
+		length++;
+	    }
 
-	ListNode curNode = head;
-	int length = 0; // start from 0
-	while (curNode != null) {
-	    length++;
+	    TreeNode result = sortedListToBST(head, length);
+
+	    return result;
 	}
 
-	TreeNode result = sortedListToBST(head, length);
+	private TreeNode sortedListToBST(ListNode head, int length) {
+	    if (length <= 0)
+		return null;
 
-	return result;
-    }
+	    int mid = (length - 1) / 2;
 
-    private TreeNode sortedListToBST(ListNode head, int length) {
-	if (length <= 0)
-	    return null;
+	    ListNode midNode = head;
+	    int counter = 0;
+	    while (counter < mid) {
+		midNode = midNode.next;
+		counter++;
+	    }
 
-	int mid = (length - 1) / 2;
+	    TreeNode root = new TreeNode(midNode.val);
+	    root.left = sortedListToBST(head, counter);
+	    root.right = sortedListToBST(midNode.next, length - 1 - counter);
 
-	ListNode midNode = head;
-	int counter = 0;
-	while (counter < mid) {
-	    midNode = midNode.next;
-	    counter++;
+	    return root;
 	}
-
-	TreeNode root = new TreeNode(midNode.val);
-	root.left = sortedListToBST(head, counter);
-	root.right = sortedListToBST(midNode.next, length - 1 - counter);
-
-	return root;
     }
 }
