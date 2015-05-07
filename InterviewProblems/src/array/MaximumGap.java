7df9048fdfa6c96419d2902c9726a99cb6b8a096
Yuan <joseftien@gmail.com>
2015-05-07 04:22:49 -0700

    2015-05-07

--- InterviewProblems/src/array/MaximumGap.java
+++ InterviewProblems/src/array/MaximumGap.java
@@ -16,6 +16,14 @@ package array;
 public class MaximumGap {
     /*
      * Bucket sort
+     * 
+     * Say there are N numbers within range A to B. The smallest/biggest numbers
+     * are of value A and B.If all the rest N-2 numbers distributed evenly then
+     * we have the smallest possible maximum gap. These N-2 numbers will form at
+     * most N-1 ranges (cut a rope N-2 times you get N-1 pieces)
+     * 
+     * To evenly cut [A,B] into N-1 buckets, each bucket has size of
+     * ceiling[(B-A)/(N-1)].
      */
     public class Solution {
 	class Range {
@@ -42,7 +50,9 @@ public class MaximumGap {
 
 	    int size = (int) Math
 		    .ceil((double) (max - min) / (nums.length - 1));
-	    Range[] buckets = new Range[(max - min) / size + 1];
+
+	    // new Range(nums.length) also works
+	    Range[] buckets = new Range[(max - min) / size + 1];	    
 	    for (int i = 0; i < nums.length; i++) {
 		int k = (nums[i] - min) / size;
 		Range r = buckets[k];
