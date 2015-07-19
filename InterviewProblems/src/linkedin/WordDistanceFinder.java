284fcb979de71a1538e75c9196e62da3057067f0
mbp-2015 <joseftien@gmail.com>
2015-07-18 20:17:56 -0700

    2015-07-18

--- InterviewProblems/src/linkedin/WordDistanceFinder.java
+++ InterviewProblems/src/linkedin/WordDistanceFinder.java
@@ -9,8 +9,29 @@ import java.util.List;
 public class WordDistanceFinder {
     class Solution {
 	public int findCloestWordDistance(String s1, String s2, List<String> words) {
+	    if (s1 == null || s2 == null || words == null) {
+		return -1;
+	    }
 
-	    return -1;
+	    int res = Integer.MAX_VALUE;
+	    int index1 = -1;
+	    int index2 = -1;
+	    
+	    for (int i = 0; i < words.size(); i++) {
+		if (words.get(i).equals(s1)) {
+		    index1 = i;
+		}
+		if (words.get(i).equals(s2)) {
+		    index2 = i;
+		}
+
+		if (index1 >= 0 && index2 >= 0) {
+		    res = Math.min(res, Math.abs(index1 - index2));
+		}
+
+	    }
+
+	    return res;
 	}
     }
 }
