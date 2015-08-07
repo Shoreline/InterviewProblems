aed15f4b3e07bf47b4bd6cc92fe901ae389dc4f0
Yuan <joseftien@gmail.com>
2015-08-07 04:49:11 -0700

    2015-08-06

--- InterviewProblems/src/string/Anagrams.java
+++ InterviewProblems/src/string/Anagrams.java
@@ -15,6 +15,46 @@ import java.util.Map;
  */
 
 public class Anagrams {
+    public class Solution {
+	private int getHash(int[] count) {
+	    int hash = 0;
+	    int a = 378551;
+	    int b = 63689;
+	    for (int num : count) {
+		hash = hash * a + num;
+		a = a * b;
+	    }
+	    return hash;
+	}
+
+	public ArrayList<String> anagrams(String[] strs) {
+	    ArrayList<String> result = new ArrayList<String>();
+	    HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+
+	    for (String str : strs) {
+		int[] count = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+		    count[str.charAt(i) - 'a']++;
+		}
+
+		int hash = getHash(count);
+		if (!map.containsKey(hash)) {
+		    map.put(hash, new ArrayList<String>());
+		}
+
+		map.get(hash).add(str);
+	    }
+
+	    for (ArrayList<String> tmp : map.values()) {
+		if (tmp.size() > 1) {
+		    result.addAll(tmp);
+		}
+	    }
+
+	    return result;
+	}
+    }
+
     /*
      * The goal is to output all Strings that has more than one anagram in given
      * String[].
@@ -22,7 +62,7 @@ public class Anagrams {
      * The sorted char array of two anagrams are the same. So use it as key to
      * group Strings.
      */
-    public class Solution {
+    public class Solution_slow {
 	public List<String> anagrams(String[] strs) {
 	    List<String> res = new ArrayList<>();
 	    if (strs == null || strs.length == 0) {
